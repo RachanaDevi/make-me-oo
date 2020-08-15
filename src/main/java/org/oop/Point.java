16a7ad84030dfcd493bc580b6116d1ec389086cf
Rachana-Devi <rachanadevi18@gmail.com>
2020-08-16 00:55:30 +0530

    [Rachana] Solve Shivam's OOP problem

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -4,24 +4,21 @@ public class Point {
     private double x;
     private double y;
 
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
+
+    public double distanceFrom(Point point){
+        double x_diff = point.x - this.x;
+        double y_diff = point.y - this.y;
+        return Math.sqrt(Math.pow(x_diff,2)+Math.pow(y_diff,2));
+    }
+
+    public double directionFrom(Point point){
+        double x_diff = point.x - this.x;
+        double y_diff = point.y - this.y;
+        return Math.atan2(y_diff,x_diff);
+    }
+
 }
